4c9385734e3a28ed26db97b6c36470f753f72bd0
Hans Häggström <zzorn@iki.fi>
2010-10-18 19:35:29 +0300

    include example also

--- package.sh
+++ package.sh
@@ -4,7 +4,7 @@ library_name=animator
 package_name=$library_name-$(cat version.txt)
 build_dir=build/$library_name
 
-echo "Packaging $package_name"
+echo "Packaging build/$package_name"
 
 mkdir -p $build_dir
 rm $build_dir/*
@@ -15,6 +15,7 @@ cp readme.txt $build_dir
 cp license.txt $build_dir
 cp version.txt $build_dir
 cp changes.txt $build_dir
+cp AnimationExample.pde $build_dir
 
 cd build
 rm $package_name.zip
